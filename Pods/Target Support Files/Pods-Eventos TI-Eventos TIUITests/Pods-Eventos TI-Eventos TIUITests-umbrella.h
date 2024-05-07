b14c0b3a33f984c2462d6a204fef02a5a7ac4c4c
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-05-07 02:03:40 -0300

    Instalando o CocoaPods

---
+++ Pods/Target Support Files/Pods-Eventos TI-Eventos TIUITests/Pods-Eventos TI-Eventos TIUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Eventos_TI_Eventos_TIUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Eventos_TI_Eventos_TIUITestsVersionString[];
+
---
+++ Pods/Target Support Files/Pods-Eventos TI/Pods-Eventos TI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Eventos_TI : NSObject
+@end
+@implementation PodsDummy_Pods_Eventos_TI
+@end
