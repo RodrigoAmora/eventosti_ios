dd43d901659fe4edac072e648c59c9de37686ea5
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-05-23 00:28:23 -0300

    Implementando a classe BaseViewController

---
+++ Eventos TI/Controller/BaseViewController.swift
@@ -0,0 +1,56 @@
+//
+//  BaseViewController.swift
+//  Eventos TI
+//
+//  Created by Rodrigo Amora on 23/05/24.
+//
+
+import Foundation
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    func changeViewControllerWithPresent(_ viewController: UIViewController) {
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
+    func changeViewControllerWithPushViewController(_ viewController: UIViewController) {
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    func getVersionApp() -> String? {
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        return appVersion
+    }
+    
+    func hideKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        let buttonOK = UIAlertAction(title: "OK", style: .cancel)
+        
+        alert.addAction(buttonOK)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showError(errorCode: Int) {
+        let message = switch errorCode {
+            case 403:
+                String(localized: "error_access_denied")
+            case 404:
+                String(localized: "error_not_found")
+            case 500:
+                String(localized: "error_service_unavailable")
+            default:
+                String(localized: "error_cant_was_possible_perform_operation")
+        }
+        
+        self.showAlert(title: "", message: message)
+    }
+    
+}
