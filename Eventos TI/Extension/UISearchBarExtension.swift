9cb14bc0132e22346a768fb4a7d40e91b86f78dc
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-15 00:56:31 -0300

    Criando a extension hide e show da classe UISearchBar

---
+++ Eventos TI/Extension/UISearchBarExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UISearchBarExtension.swift
+//  Eventos TI
+//
+//  Created by Rodrigo Amora on 15/08/24.
+//
+
+import Foundation
+import UIKit
+
+extension UISearchBar {
+    func hide() {
+        self.isHidden = true
+    }
+    
+    func show() {
+        self.isHidden = false
+    }
+}
