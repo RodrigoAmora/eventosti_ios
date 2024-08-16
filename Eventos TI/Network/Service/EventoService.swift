58094cda326c06edac43f6e37946a4c04edeb79d
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-16 01:20:51 -0300

    Ajustando o endpoint da busca de evento por nome

--- Eventos TI/Network/Service/EventoService.swift
+++ Eventos TI/Network/Service/EventoService.swift
@@ -20,7 +20,7 @@ class EventoService {
                    encoding: URLEncoding.default)
             .response{ response in
                 switch response.result {
-                    case .success(let json):
+                case .success(_):
                         switch response.response?.statusCode {
                             case 200:
                                 guard let data = response.data else { return }
@@ -51,22 +51,22 @@ class EventoService {
                                 break
                         }
                     
-                    case .failure(let error):
+                case .failure(_):
                         completion([], 0)
                         break
                 }
             }
     }
     
-    func buscarEventosPeloNome(nome: String, page: Int, completion: @escaping(_ eventos: [Evento], _ error: Int?) -> Void) {
-        let path = "\(self.baseURL)/api/evento/\(nome)/buscarPorNome?page=\(page)"
+    func buscarEventosPeloNome(nome: String, page: Int, completion: @escaping(_ eventos: [Evento]?, _ error: Int?) -> Void) {
+        let path = "\(self.baseURL)/api/evento/buscarPorNome?nome=\(nome)&page=\(page)"
         
         AF.request(path,
                    method: .get,
                    encoding: URLEncoding.default)
             .response{ response in
                 switch response.result {
-                    case .success(let json):
+                case .success(_):
                         switch response.response?.statusCode {
                             case 200:
                                 guard let data = response.data else { return }
@@ -76,29 +76,33 @@ class EventoService {
                                     completion(eventos, nil)
                                 } catch {
                                     print("Error retriving questions \(error)")
-                                    completion([], 0)
+                                    completion(nil, 0)
                                 }
                                 break
                             
                             case 401:
-                                completion([], 401)
+                                completion(nil, 401)
                                 break
                             
                             case 403:
+                                completion(nil, 403)
+                                break
+                            
+                            case 404:
                                 completion([], 403)
                                 break
                             
                             case 500:
-                                completion([], 500)
+                                completion(nil, 500)
                                 break
                             
                             default:
-                                completion([], 0)
+                                completion(nil, 0)
                                 break
                         }
                     
-                    case .failure(let error):
-                        completion([], 0)
+                    case .failure(_):
+                        completion(nil, 0)
                         break
                 }
             }
