076e0db9ed56d63363367b86756573c36c7e7d9e
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-05-28 00:42:11 -0300

    Método posY

--- Eventos TI/Extension/UISearchBarExtension.swift
+++ Eventos TI/Extension/UISearchBarExtension.swift
@@ -16,4 +16,19 @@ extension UISearchBar {
     func show() {
         self.isHidden = false
     }
+    
+    func posY() {
+        let searchBarPosY: CGFloat = switch UIDevice.current.userInterfaceIdiom {
+            case .pad:
+                75
+            
+            case .phone:
+                95
+            
+            default:
+                85
+        }
+        
+        self.frame.origin.y = searchBarPosY
+    }
 }
